5b0bcced95eeadae38a01f8262c44690b6084b83
Gabor Gulyas <guyko81@gmail.com>
2025-12-11 13:40:24 +0000

    Update version to 1.3.0 in pyproject.toml and setup.py
    - Incremented version number to 1.3.0 in both `pyproject.toml` and `setup.py` to reflect the latest changes and improvements in the distribution-regressor package.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="distribution-regressor",
-    version="1.2.0",
+    version="1.3.0",
     author="Gabor Gulyas",
     author_email="",
     description="Nonparametric distributional regression using LightGBM",
